940085bee690d14705a8f8c9d05d67f6a21fc3a7
Urbain17 <messanvitr@gmail.com>
2019-11-07 21:18:48 -0500

    Envoie du chapitre 1

---
+++ Programmation1/TechniquesProgrammationWeb1/js/FonctionQuadratique.js
@@ -0,0 +1,16 @@
+/*Fonction quadratique.
+Lusager entre A, B, C, X, vous trouvez Y.
+Rappel : Y = AX2 + BX +
+ */
+
+var A, B, C, Y;
+
+//Lecture des variables
+A = Number(prompt("Entrez  la valeur de A"));
+B = Number(prompt("Entrez  la valeur de B"));
+C = Number(prompt("Entrez  la valeur de C"));
+X = Number(prompt("Entrez  la valeur de X"));
+
+// Y = AX2 + BX + C
+
+
---
+++ Programmation1/TechniquesProgrammationWeb1/js/exercice1.js
@@ -0,0 +1,17 @@
+/*
+*Par Amentor Messanvi
+ */
+var nom;
+
+//Opération de lecture (input / entrée)
+//L'usager va nous donner le nom
+
+nom = prompt(" Entrez votre nom svp: ");
+//Shift+2
+//Shift-droite + .
+//shift-droite + à côté du L """""""""""
+
+//Opération d'affichage (sortie / output / écrire)
+console.log("Helllo Input/Output");
+console.log(nom);
+console.log("Bonjour " + nom);
