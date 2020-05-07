88f426329063119d057292faf3158f0bd099efd9
Urbain17 <messanvitr@gmail.com>
2020-05-06 23:51:31 -0400

    becflix3

--- Programmation2React/becflix/src/components/TvShow/TvShow.js
+++ Programmation2React/becflix/src/components/TvShow/TvShow.js
@@ -1,27 +1,26 @@
+  
 import React from "react";
 import Col from "react-bootstrap/Col";
 import showImage from "../../img/lesinvincibles.jpg";
 
 let image = {
-    scr: showImage,
-    alt: "Les invincibles",
-    title: "Les invincibles",
+  src: showImage,
+  alt: "Les invincibles",
+  title: "Les invicibles",
 };
 
-
-
-export class TvShow extends React.Component{
-    render(){
-        console.log(this.props.nom);
-        return(
-            <Col xs lg="2">
-                <img
-                scr={image.scr}
-                alt={image.alt}
-                title={image.title}
-                className="img-fluid"
-                />
-            </Col>
-        );
-    }
+export class TvShow extends React.Component {
+  render() {
+    console.log(this.props.nom);
+    return (
+      <Col xs lg="2">
+        <img
+          src={image.src}
+          alt={image.alt}
+          title={image.title}
+          className="img-fluid"
+        />
+      </Col>
+    );
+  }
 }
