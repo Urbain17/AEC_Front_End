fb225134bc468ae1380af121fb5b8a40a72b0a41
Urbain17 <57510471+Urbain17@users.noreply.github.com>
2020-06-09 20:19:28 -0400

    Add files via upload

---
+++ Programmation2React/projet1/build/precache-manifest.f9ad3cbf39dbd6078222d76c5b2f7904.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08c65ae581126bb136b819221bc5174",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ddd49047ac928051de1e",
+    "url": "./static/css/2.92a0e85f.chunk.css"
+  },
+  {
+    "revision": "2b7b4223226c0f5568cb",
+    "url": "./static/css/main.9d519632.chunk.css"
+  },
+  {
+    "revision": "ddd49047ac928051de1e",
+    "url": "./static/js/2.6aa4ce9f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.6aa4ce9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7b4223226c0f5568cb",
+    "url": "./static/js/main.1fd1a076.chunk.js"
+  },
+  {
+    "revision": "7cc95af01ac3349ab9ab",
+    "url": "./static/js/runtime-main.999fee6a.js"
+  },
+  {
+    "revision": "b2daf53d0940a530af9fe26f43082f1b",
+    "url": "./static/media/2Freres_Nous_autres.b2daf53d.jpg"
+  },
+  {
+    "revision": "6c6a11d8922589390c7ba68f02a2587a",
+    "url": "./static/media/Alex Nevsky_Himalaya_mon_amour.6c6a11d8.jpg"
+  },
+  {
+    "revision": "d92d9ecafabaf09e63b402df8569d38a",
+    "url": "./static/media/ImageSpotify.d92d9eca.png"
+  },
+  {
+    "revision": "0fd398c36612353b31793f867d3933bf",
+    "url": "./static/media/Karim Ouellet_Fox.0fd398c3.jpg"
+  },
+  {
+    "revision": "c0b7d4fef29b36e61cf612166a40acce",
+    "url": "./static/media/Klo Pelgag_Alchimie_des_monstres.c0b7d4fe.jpg"
+  },
+  {
+    "revision": "9476774f547c2415f66e0837abfc78a8",
+    "url": "./static/media/Les cowboys fringants_Les_antipodes.9476774f.jpg"
+  },
+  {
+    "revision": "82c45042d82f196e230b6b3a6d6ce93d",
+    "url": "./static/media/Safia Nolin_Limoilou.82c45042.jpg"
+  }
+]);
