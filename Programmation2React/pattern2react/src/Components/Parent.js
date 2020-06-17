5d4a2fe89e31c1f10a9de698604a3bdd637372f7
Urbain17 <messanvitr@gmail.com>
2020-06-16 21:51:18 -0400

    envoie pattern2react

---
+++ Programmation2React/pattern2react/src/Components/Parent.js
@@ -0,0 +1,50 @@
+import React from 'react'; 
+
+import ReactDOM from 'react-dom'; 
+
+import { Child } from './Child'; 
+
+import { Ami } from './Ami'; 
+
+ 
+ 
+
+export class Parent extends React.Component { 
+
+  constructor(props) { 
+
+    super(props); 
+
+    this.state = { name: 'Patate' }; 
+
+    this.changeName = this.changeName.bind(this); 
+
+} 
+
+ 
+ 
+
+  changeName(newName) { 
+
+    this.setState({name: newName}); 
+
+  } 
+
+ 
+  render() { 
+
+    return ( 
+
+      <> 
+
+        <Child OnChange={this.changeName} /> 
+
+        <Ami name={this.state.name} /> 
+
+      </> 
+
+    ); 
+
+  } 
+
+} 
